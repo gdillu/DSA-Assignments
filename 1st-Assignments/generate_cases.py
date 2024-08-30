c526d01988aceec19b79f7d7f3523f552951d56b
gdillu <74751510+gdillu@users.noreply.github.com>
2024-08-30 15:33:42 +0530

    generate_cases.py

--- 1st-Assignments/generate_cases.py
+++ 1st-Assignments/generate_cases.py
@@ -1,17 +1,20 @@
 import random
 
 # Function to generate random test cases
+
 def random_test_cases(n):
-    test_cases = [str(random.randint(0, 1000000)) for _ in range(n)]
-    return "\n".join(test_cases)
+    test_cases = [random.randint(0, 1000000) for _ in range(n)]
+    return "\n".join(map(str, test_cases))
+
 def Best_test_cases(n):
-    test_cases = [str(random.randint(0, 1000000)) for _ in range(n)]
+    test_cases = [random.randint(0, 1000000) for _ in range(n)]
     test_cases.sort()
-    return "\n".join(test_cases)
+    return "\n".join(map(str, test_cases))
+
 def Worst_test_cases(n):
-    test_cases = [str(random.randint(0, 1000000)) for _ in range(n)]
+    test_cases = [random.randint(0, 1000000) for _ in range(n)]
     test_cases.sort(reverse=True)
-    return "\n".join(test_cases)
+    return "\n".join(map(str, test_cases))
 
 # Define the sizes you want to generate
 sizes = [100, 250, 500,750,1000,2500,5000,7500,10000,25000,50000,75000,100000]
